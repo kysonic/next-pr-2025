fcb062c8e86771abce4813434ec594153bb35557
kysonic <soooyc@gmail.com>
2025-10-23 16:47:21 +0300

    chore: flowbite

---
+++ src/components/layouts/MainLayout.tsx
@@ -0,0 +1,34 @@
+import Head from 'next/head'
+import MainHeader from './headers/MainHeader'
+
+interface LayoutProps {
+  children: React.ReactNode
+  title?: string
+}
+
+export default function Layout({ 
+  children, 
+  title = 'Next Book Shop' 
+}: LayoutProps) {
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content="Next.js Page Router website" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      
+      <div className="main-layout">
+        <MainHeader />
+        
+        <main className="main">
+          {children}
+        </main>
+        
+        <footer className="footer">
+          <p>&copy; 2024 My Website. All rights reserved.</p>
+        </footer>
+      </div>
+    </>
+  )
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,21 @@
 import type { AppProps } from 'next/app';
+import type { ReactElement, ReactNode } from 'react';
+import type { NextPage } from 'next';
+import MainLayout from '@/components/layouts/MainLayout';
+
 import '@/styles/globals.css';
 
-export default function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode
+}
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
+
+export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  // Use the layout defined at the page level, if available
+  const getLayout = Component.getLayout ?? ((page) => <MainLayout>{page}</MainLayout>)
+
+  return getLayout(<Component {...pageProps} />)
 }
